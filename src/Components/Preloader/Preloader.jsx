bba4633653b371e46cd9ee63d54e7321e7fd1f58
Jansen Stanlie <81914393+Jansen-Stanlie@users.noreply.github.com>
2024-05-28 00:20:38 +0700

    update

--- src/Components/Preloader/Preloader.jsx
+++ src/Components/Preloader/Preloader.jsx
@@ -5,7 +5,7 @@ const Preloader = ({ varient }) => {
     <div className={`st-perloader ${varient ? varient : ""}`}>
       <div className="loader-container">
         <img src="/images/Ikon PK Warna.png" alt="Loader Image" className="loader-image" />
-        <div className="loader-text">PANTAI TUBAN</div>
+        <div className="loader-text">Pantai Tuban</div>
       </div>
     </div>
   );
